78710504698ea68816f1c2314fa5e15b9455662e
User <user@Users-MacBook-Pro.local>
2023-03-07 01:56:32 +0100

    çorrectiono1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,7 @@
-/*                                                                                                                                   
- * File: 0-memset.c                                                                                                                  
+/*                                                                              * File: 0-memset.c                                                                                                                  
  * Auth: not                                                                                                                         
- */                                                                                                                                  
-                                                                                                                                     
-#include "main.h"                                                                                                                    
-                                                                                                                                     
+ */
+#include "main.h"
 /**                                                                                                                                  
  * _memset - Fills the first n bytes of the memory area                                                                              
  *          pointed to by @s with the constant byte @c.                                                                              
@@ -15,14 +12,10 @@
  *                                                                                                                                   
  * Return: A pointer to the filled memory area @s.                                                                                   
  *                                                                                                                                   
- */                                                                                                                                  
-void *_memset(void *s, int c, size_t n)                                                                                              
-{                                                                                                                                    
-        unsigned int index;                                                                                                          
-        unsigned char *memory = s, value = c;                                                                                        
-                                                                                                                                     
-        for (index = 0; index < n; index++)                                                                                          
-                memory[index] = value;                                                                                               
-                                                                                                                                     
-        return (memory);                                                                                                             
+ */                                                                             void *_memset(void *s, int c, size_t n)                                         {
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+	return (memory);
 }
